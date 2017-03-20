452ad1e580f5a84b6a9a28c5e00b87e4347ee2fb
SimonaArsova <simona.liubomirova@gmail.com>
2017-03-20 21:13:55 +0200

    Change button image when there's no sound

--- sound-manipulation.js
+++ sound-manipulation.js
@@ -2,13 +2,16 @@ function regulateSound(){
 
     let soundButton = document.getElementById('soundBtn');
     let audioElement = document.getElementById('music');
+    let onImage = document.getElementById('soundOn');
 
     soundButton.addEventListener('click', function(){
         if(!audioElement.muted){
             audioElement.muted = true;
+            onImage.src = "./images/soundOff.png";
         }
         else{
             audioElement.muted = false;
+            onImage.src = "./images/sound.png";
         }
     });
 }
